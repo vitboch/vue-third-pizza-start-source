f115bfcd0d7f979f08182518d81593fdd352534b
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-10-09 10:33:45 +0700

    section2

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,10 +1,10 @@
+<script setup>
+import AppHeader from "./AppHeader.vue";
+</script>
+
 <template>
   <div>
     <app-header />
     <slot />
   </div>
 </template>
-
-<script setup>
-import { AppHeader } from "@/components";
-</script>
